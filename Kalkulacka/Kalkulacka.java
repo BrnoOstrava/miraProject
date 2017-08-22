bb7f4b7051c8f986041c71cfb80d9a7e350e2841
Miiro <koutny.mk@seznam.cz>
2017-08-22 19:33:16 +0200

    Pripraveni metod

---
+++ Kalkulacka/Kalkulacka.java
@@ -0,0 +1,46 @@
+public class Kalkulacka{
+    public int scitani(int a, int b){
+        return 0;
+    }
+    
+    public int odcitani(int a, int b){
+        return 0;
+    }
+    
+    public int nasobeni(int a, int b){
+        return 0;
+    }
+    
+    public double deleni(double a, double b){
+        return 0;
+    }
+    
+    public String deleniSeZbytkem(int a, int b){
+        return null;
+    }
+    
+    public String prevodDoDvojkoveSoustavy(int a){
+        return null;
+    }
+    
+    public int prevodDoDesitkoveSoustavy(String desitkove){
+        return 0;
+    }
+    
+    public int mocnina(int a, double b){
+        return 0;
+    }
+    
+    public int mocninaNaDruhou(int a){
+        return 0;
+    }
+    
+    public double odmocnina(int a){
+        return 0;
+    }
+    
+    public int faktorial (int a){
+        return 0;
+    }
+        
+}
